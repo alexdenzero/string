ebea5054806bc23f080647784ad696e00e80eea1
alexdenzero <alexdenzero@yandex.ru>
2023-03-21 16:55:06 +0300

    string

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        String firstName = "ivan";
+        String middleName = "ivanov";
+        String lastName = "ivanovich";
+        String fullName = middleName + " " + firstName + " " + lastName;
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        String firstName = "ivan";
+        String middleName = "ivanov";
+        String lastName = "ivanovich";
+        String fullName = middleName + " " + firstName + " " + lastName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
